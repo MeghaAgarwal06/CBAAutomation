fb61f4b5cd6ec7903b15e41ec44844a27e61e9d8
MeghaAgarwal06 <meg.agrwal@gmail.com>
2021-02-02 01:24:45 +1100

    added test cases and config

---
+++ src/test/java/com/cba/testCases/TC_TravelMoney.java
@@ -0,0 +1,64 @@
+package com.cba.testCases;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.cba.pageObjects.LogOnPage;
+import com.cba.pageObjects.LoginPage;
+import com.cba.pageObjects.TravelMoneyCardApply;
+import com.cba.pageObjects.TravelMoneyPage;
+import com.cba.pageObjects.TravelPage;
+
+public class TC_TravelMoney extends BaseClass {
+
+	
+	@Test
+	public void testTravelMoney()  {
+		//Navigate to https://www.commbank.com.au/travel.html
+		driver.get(baseUrl);
+		 driver.manage().window().maximize();
+		
+		TravelPage travel = new TravelPage(driver);
+		//Click on discover more
+		travel.clickDiscoverMore();
+		
+		TravelMoneyPage travelPage = new TravelMoneyPage(driver);
+		List<String> btns= travelPage.checkOptions();
+		//Check the 2 option available
+		ArrayList<String> expectedArr = new ArrayList<String>(2);
+		expectedArr.add("Foreign exchange calculator");
+		expectedArr.add("Exchange rates");
+		 Iterator<String> itr=btns.iterator();//getting the Iterator  
+		  while(itr.hasNext()){ 
+			  boolean ans = expectedArr.contains(itr.next()); 
+			  Assert.assertTrue(ans);			  	   	  
+		  }
+		
+
+		 
+		  travelPage.clickRegisterOnline(); 
+		  // Switch to new window opened
+		  for(String winHandle : driver.getWindowHandles()){
+		      driver.switchTo().window(winHandle);
+		  }
+		  
+		  TravelMoneyCardApply applyOnline = new TravelMoneyCardApply(driver);
+		  applyOnline.clickOnlineOption();
+		  driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+		 
+		  LogOnPage logOn = new LogOnPage(driver);
+		  
+		  boolean expected =logOn.checkClientNumberFieldPresent();
+		  Assert.assertEquals(true, expected);
+		  expected=logOn.checkPasswordFieldPresent();
+		  Assert.assertEquals(true, expected);
+		   
+		  
+}
+}
